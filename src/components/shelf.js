ab01f5dc8ea7b2849cc51116b81bfa8def4bc0cf
abrarfaiazatuno <abrarfaiaz0@gmail.com>
2023-02-04 01:46:59 +0600

    reverted sliderjs

--- src/components/shelf.js
+++ src/components/shelf.js
@@ -1,16 +1,19 @@
 import Card from "./card";
 import styles from "@/styles/Shelf.module.css";
-import { Swiper, SwiperSlide } from "swiper/react";
-
-import "swiper/css";
 
 const Shelf = () => {
   return (
     <div className={styles.shelf}>
-      <Swiper>
-        <SwiperSlide>assd</SwiperSlide>
-        <SwiperSlide>assdasssds</SwiperSlide>
-      </Swiper>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
     </div>
   );
 };
