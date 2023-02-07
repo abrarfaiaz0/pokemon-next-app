236dc9d6c465d33beb5fdb97dca081c15017cc3b
abrarfaiazatuno <abrarfaiaz0@gmail.com>
2023-02-07 17:01:24 +0600

    added blog section

--- src/components/blog.js
+++ src/components/blog.js
@@ -14,8 +14,127 @@ const Blog = () => {
           Ash & Pikachu Arrive in <br></br>Pokemon Universe
         </div>
         <div className={styles.blogtext}>
-          <div className={styles.blogtextleft}></div>
-          <div className={styles.blogtextright}></div>
+          <div className={styles.blogtextleft}>
+            <div className={styles.lefttop}>
+              <div className={styles.three}>
+                <div>
+                  Integer elit quam, vestibulum sit amet justo elementum,
+                  pulvinar finibus lectus. In posuere odio quam, sagittis
+                  egestas mi convallis id. Aliquam pulvinar augue eget metus
+                  luctus interdum vel vitae turpis. In vulputate magna vel
+                  tempus maximus. In hac habitasse platea dictumst. Cras eget
+                  hendrerit tortor. Phasellus interdum ligula vel justo
+                  tristique vestibulum vitae eu tellus.
+                </div>
+                <div className={styles.two}>
+                  <div>
+                    Aliquam eu quam viverra, semper nisl vel, malesuada velit.
+                    Nam blandit in lorem et lobortis. Pellentesque vel nisi
+                    enim. Suspendisse potenti. Sed elementum tincidunt varius.
+                    Maecenas eu nulla porttitor, hendrerit nisl non, placerat
+                    libero.Duis euismod est a lorem semper, nec sodales nisl
+                    dignissim. Integer dignissim, velit eu imperdiet finibus,
+                    erat odio maximus erat, eget pharetra enim libero vitae
+                    ex.Vestibulum mattis luctus turpis, at venenatis velit
+                    consequat eu. Orci varius natoque penatibus et magnis dis
+                    parturient montes, nascetur ridiculus mus. Suspendisse
+                    imperdiet ut tellus non gravida. Etiam velit libero, rutrum
+                    ac pretium in, sollicitudin sed erat. Donec et est neque.
+                    Aenean sagittis sagittis bibendum.
+                  </div>
+                  <div className={styles.divimg}>
+                    <img src="./assets/Image04.png"></img>
+                  </div>
+                </div>
+                <div>
+                  Maecenas volutpat purus ac sapien viverra porttitor. Aliquam
+                  at metus imperdiet, congue leo nec, feugiat justo. Aliquam
+                  orci nunc, ullamcorper non lacinia et, consequat iaculis arcu.
+                  Donec sed augue tortor. Lorem ipsum dolor sit amet,
+                  consectetur adipiscing elit. Phasellus imperdiet,
+                </div>
+              </div>
+              <div className={styles.twoimg}>
+                <div className={styles.divimg}>
+                  <img src="./assets/Image03.png"></img>
+                </div>
+                <div className={styles.divimg}>
+                  <img src="./assets/Image02.png"></img>
+                </div>
+              </div>
+            </div>
+            <div className={styles.leftbottom}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac
+              elementum augue. Quisque porttitor finibus nulla. Aliquam
+              convallis maximus massa eu consectetur. Fusce eget turpis ut purus
+              rutrum varius. Cras enim ante, ultrices non urna a, maximus mattis
+              metus. Morbi mattis, tellus ac fringilla feugiat, justo dolor
+              tempus mi, a imperdiet sapien sapien non tellus. Etiam a lacus
+              neque. Pellentesque consectetur ligula a mattis porttitor. Cras
+              lectus turpis, tempus ac augue maximus, sollicitudin ultricies
+              arcu. Praesent mattis tristique leo. Cras convallis odio non
+              lectus semper, et suscipit nibh consequat. Morbi faucibus volutpat
+              arcu vel pellentesque. Phasellus sodales magna at massa pharetra,
+              eu eleifend arcu interdum. Ut pretium placerat odio, ultrices
+              sollicitudin quam tempor quis. Maecenas viverra ipsum sed
+              vulputate euismod. Mauris neque ante, tempor in eleifend eu,
+              eleifend eleifend ipsum.
+            </div>
+          </div>
+          <div className={styles.blogtextright}>
+            <div>
+              Quisque dictum eleifend efficitur. Duis ornare, justo a viverra
+              sagittis, ligula odio luctus arcu, non vehicula justo tortor non
+              mi. Donec eleifend mollis felis, id euismod lectus tristique
+              condimentum. Aenean lobortis
+            </div>
+            <div className={styles.second}>
+              <div className={styles.divimg}>
+                <img src="./assets/Image05.png"></img>
+              </div>
+              <div>
+                quam bibendum metus mattis bibendum. Vestibulum convallis a
+                neque sit amet pretium. Pellentesque dapibus ante consequat,
+                bibendum quam vel, aliquet lacus. Pellentesque habitant morbi
+                tristique senectus et netus et malesuada fames ac turpis
+                egestas. Praesent mattis tristique leo. Cras convallis odio non
+                lectus semper, et suscipit nibh consequat. Morbi faucibus
+                volutpat arcu vel pellentesque.Nunc ut leo auctor, rhoncus urna
+                id, luctus justo. Curabitur hendrerit justo velit, vitae tempor
+                tellus faucibus quis. Aliquam quis egestas eros
+              </div>
+            </div>
+            <div className={styles.third}>
+              <div className={styles.divimg}>
+                <img src="./assets/Image01.png"></img>
+              </div>
+              <div>
+                Maecenas viverra ipsum sed vulputate euismod. Mauris neque ante,
+                tempor in eleifend eu, eleifend eleifend ipsum. Integer elit
+                quam, vestibulum sit amet justo elementum, pulvinar finibus
+                lectus. In posuere odio quam, sagittis egestas mi convallis id.
+                Aliquam pulvinar augue eget metus luctus interdum vel vitae
+                turpis. In vulputate magna vel tempus maximus. In hac habitasse
+                platea dictumst.
+              </div>
+            </div>
+            <div>
+              sed consectetur eu, scelerisque at est. Aliquam congue lorem vel
+              risus lacinia pellentesque. Aliquam eu quam viverra, semper nisl
+              vel, malesuada velit. Nam blandit in lorem et lobortis.
+              Pellentesque vel nisi enim. Suspendisse potenti. Sed elementum
+              tincidunt varius. Maecenas eu nulla porttitor, hendrerit nisl non,
+              placerat libero. Ut tempor ipsum ut mauris consectetur
+              sollicitudin. Ut aliquam eros ac lacus dapibus efficitur. Quisque
+              at imperdiet arcu, eu scelerisque est. Praesent erat libero,
+              placerat vel lacinia sed, mollis sit amet lacus. Curabitur
+              fringilla massa nisl, nec commodo neque gravida et. Curabitur
+              vehicula accumsan magna in tempus. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Maecenas rutrum justo sodales
+              malesuada vestibulum. Phasellus porta tempus leo, non consequat
+              elit. Integer aliquet tincidunt purus at vulputate.
+            </div>
+          </div>
         </div>
       </div>
       <div className={styles.left}>
